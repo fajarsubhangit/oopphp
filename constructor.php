9d54280eb412fef223ee5b58857333c272e41e58
fajarsubhan <fajarsubhan9b@gmail.com>
2019-07-24 22:57:39 +0700

    update commit

--- constructor.php
+++ constructor.php
@@ -1,11 +1,11 @@
 <?php
 class Produk {
-  public $judul = "judul";
-  public $penulis = "penulis";
-  public $penerbit = "penerbit";
-  public $harga = 0;
+  public $judul;
+  public $penulis;
+  public $penerbit;
+  public $harga;
 
-  public function __construct($judul,$penulis,$penerbit,$harga) {
+  public function __construct($judul="judul1",$penulis="penulis1",$penerbit="penerbit3",$harga) {
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
@@ -22,7 +22,17 @@ class Produk {
 $produk1 = new Produk("Naruto","Masashi","Shonen Jump",30000);
 //object 2 GAME
 $produk2 = new Produk("harvet moon","Neil","Airlanga",10000);
+//object 3 Progarming
+$produk3 = new Produk("ini judul");
 
+echo "Komik = $produk1->penulis, $produk1->penerbit";
 
+echo "<hr>";
+
+echo "Game = $produk2->penulis, $produk2->penerbit";
+
+echo "<hr>";
+
+echo "HAI = $produk3->judul,$produk3->penulis,penerbit = $produk3->penerbit";
 
  ?>
